0ddafe1f77bf13753faecbd6cd98a1f7009dc8c0
Nikolai52430 <61824121+Nikolai52430@users.noreply.github.com>
2021-01-28 21:52:38 +0300

    v0.12

--- js/main.js
+++ js/main.js
@@ -20,5 +20,38 @@ $(document).ready(function () {
 });
 
 
+const  slider = document.querySelector('.turnabout');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown',(e)=>{
+    isDown  = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+    console.log(startX);
+});
+slider.addEventListener('mouseleave',()=>{
+    isDown  = true;
+});
+slider.addEventListener('mouseup',()=>{
+    isDown = false;
+});
+slider.addEventListener('mousemove',(e)=>{
+    if(!isDown) return;
+    e.preventDefault();
+    const  x = e.pageX - slider.offsetLeft;
+    const  walk = x - startX;
+    slider.scrollLeft = scrollLeft - walk;
+
+});
+
+
+
+
+
+
+
 
 
